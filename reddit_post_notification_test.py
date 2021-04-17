2070b274d6181db4c481ded9c472da24e5ed5aee
tcm5343 <tcm5343@no.co>
2021-04-16 23:00:23 -0400

    fixed unit tests

--- reddit_post_notification_test.py
+++ reddit_post_notification_test.py
@@ -1,4 +1,4 @@
-import datetime
+import time
 from types import SimpleNamespace
 
 import pytest
@@ -10,7 +10,7 @@ import reddit_post_notification as r
 
 @pytest.fixture
 def get_time_stamp() -> str:
-    return r.get_time_stamp(datetime.datetime.now())
+    return r.get_time_stamp(time.time())
 
 
 @pytest.mark.parametrize("keyword_list, test_string, expected_result", [
@@ -45,15 +45,15 @@ def test_determine_who_to_notify(single_filter, expected_result):
     assert r.determine_who_to_notify(single_filter) == expected_result
 
 
-@pytest.mark.parametrize("time, expected_result", [
-    (datetime.datetime(2020, 3, 11, 13, 0, 0), "03-11-2020 01:00:00 PM"),
-    (datetime.datetime(1999, 7, 11, 0, 0, 0), "07-11-1999 12:00:00 AM"),
-    (datetime.datetime(2020, 3, 11, 12, 0, 0), "03-11-2020 12:00:00 PM"),
-    (datetime.datetime(2020, 3, 11, 23, 0, 0), "03-11-2020 11:00:00 PM"),
-    (datetime.datetime(2020, 3, 9, 7, 1, 0), "03-09-2020 07:01:00 AM")
+@pytest.mark.parametrize("received_result, expected_result", [
+    (time.mktime(time.strptime("2020-3-11 13:0:0", "%Y-%m-%d %H:%M:%S")), "03-11-2020 01:00:00 PM"),
+    (time.mktime(time.strptime("1999-7-11 0:0:0", "%Y-%m-%d %H:%M:%S")), "07-11-1999 12:00:00 AM"),
+    (time.mktime(time.strptime("2020-3-11 12:0:0", "%Y-%m-%d %H:%M:%S")), "03-11-2020 12:00:00 PM"),
+    (time.mktime(time.strptime("2020-3-11 23:0:0", "%Y-%m-%d %H:%M:%S")), "03-11-2020 11:00:00 PM"),
+    (time.mktime(time.strptime("2020-3-9 7:1:43", "%Y-%m-%d %H:%M:%S")), "03-09-2020 07:01:43 AM")
     ])
-def test_get_time_stamp(time, expected_result):
-    assert r.get_time_stamp(time) == expected_result
+def test_get_time_stamp(received_result, expected_result):
+    assert r.get_time_stamp(received_result) == expected_result
 
 
 @pytest.mark.parametrize("post, subreddit, expected_result", [
@@ -61,7 +61,7 @@ def test_get_time_stamp(time, expected_result):
     ({"title": ""}, "GuitarSwap", " - GuitarSwap - "),
     ({"title": "[WTS] Guitar"}, "", " -  - [WTS] Guitar")
     ])
-def test_create_result_output(post, subreddit, expected_result):
+def test_create_result_output(post, subreddit, expected_result, get_time_stamp):
     post_obj = SimpleNamespace(**post)
-    expected_result = get_time_stamp() + expected_result
+    expected_result = get_time_stamp + expected_result
     assert r.create_result_output(post_obj, subreddit) == expected_result
