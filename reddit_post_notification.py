e2f391a72702a4ab5be3f8fc14635cc2e06bed9b
no <gunstuff1@tutanota.com>
2021-04-24 19:16:50 -0400

    refactor

--- reddit_post_notification.py
+++ reddit_post_notification.py
@@ -196,7 +196,7 @@ def parse_title_for_want(post_title: str):
 
 def handle_filter_attributes(attribute: str, title: str, val: list) -> bool:
     try:
-        result = {
+        return {
             "includes": string_contains_every_element_in_list(val, title),
             "excludes": not string_contains_an_element_in_list(val, title),
             "have": string_contains_every_element_in_list(val, parse_title_for_have(title)),
@@ -206,8 +206,6 @@ def handle_filter_attributes(attribute: str, title: str, val: list) -> bool:
     except KeyError:
         print("The config includes unsupported attributes")
         sys.exit()
-
-    return result
 
 
 def filter_post(post, single_filter: dict, queue):
