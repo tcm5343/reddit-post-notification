31712bb983b5f1cc76b59a4354996d3a2d65bf5a
tcm5343 <48961675+tcm5343@users.noreply.github.com>
2024-06-09 22:52:26 -0500

    start config test

--- tests/config/config_test.py
+++ tests/config/config_test.py
@@ -2,28 +2,69 @@ from unittest.mock import patch, PropertyMock, mock_open
 
 import pytest
 
-from config import config
+from config.config import Config
+from config import config as config_file
 
 
 @pytest.fixture(autouse=True, name='mock_submission')
 def f_mock_submission():
-    with patch.object(config, 'Submission') as mock:
+    with patch.object(config_file, 'Submission') as mock:
         yield mock
 
 
 @pytest.fixture(autouse=True, name='mock_submission_filter')
 def f_mock_submission_filter():
-    with patch.object(config, 'SubmissionFilter') as mock:
+    with patch.object(config_file, 'SubmissionFilter') as mock:
         yield mock
 
 
 @pytest.fixture(autouse=True, name='mock_telegram_notifier')
 def f_mock_telegram_notifier():
-    with patch.object(config, 'TelegramNotifier') as mock:
+    with patch.object(config_file, 'TelegramNotifier') as mock:
         yield mock
 
 
-@pytest.fixture(name='mock_open')
+@pytest.fixture(autouse=True, name='mock_open')
 def f_mock_open():
-    with patch("builtins.open", mock_open(read_data="data")) as mock:
-        assert open("path/to/open").read() == "data"
+    data = """
+    creds:
+        notifiers:
+            telegram:
+                token: some-token
+    reddit:
+        client_id: some-client-id
+        client_secret: some-client-secret
+        password: some-password
+        username: some-username
+    who:
+      telegram:
+        jack: some-telegram-user-id
+    filters:
+      filter1:
+        title:
+          includes:
+            - [wts]
+        notify:
+          telegram:
+            - jack
+      filter2:
+        title:
+          includes:
+            - [wts]
+        notify:
+          telegram:
+            - jack
+    search:
+      universal:
+        - filter2
+      subreddits:
+        subreddit1:
+          - filter1
+    """
+    with patch('builtins.open', mock_open(read_data=data)) as mock:
+        yield mock
+
+
+def test_config():
+    config = Config('some/path')
+    assert True
